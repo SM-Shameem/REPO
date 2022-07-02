1afe399e9b5cfe765e22257ddd1baabaee7dd349
SM-Shameem <rony.shameem@gmail.com>
2022-07-02 12:20:09 +0600

    update 1 - adding HardvardX Certification | Coding - JS Content | Design - Tool Content

---
+++ S01-CODING/JS-PRACTICE/practice-aa.js
@@ -0,0 +1,23 @@
+///// FUNCTION INTRO ///////////////////////////////////////////////////////////
+// 01. javascript function and syntax
+// 02. function invocation
+// 03. function return
+// 04. why function
+// 05. the () operator invokes the function
+// 06. functions used as variable values
+// 07. local variables
+
+////////////////////////////////////////////////////////////////////////////////
+var outAA = document.createElement('div');
+outAA.setAttribute('id', 'styleOne');
+document.body.appendChild(outAA);
+
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+outAA.innerHTML += 'ex-aa : function and why | function syntax | function return';
+outAA.innerHTML += '<hr >'; ExAA();
+outAA.innerHTML += '<br >ex-ab : function invocation | The () Operator Invokes the Function';
+outAA.innerHTML += '<hr >'; ExAB();
+outAA.innerHTML += '<br >ex-ac : functions used as variable values | local variables';
+outAA.innerHTML += '<hr >'; ExAC();
